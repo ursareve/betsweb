3a6a57a7080147b057b9f5eecfbab53069b2eac8
Carlos Monsalve <csueskun@gmail.com>
2026-01-23 15:24:22 -0500

    feat(deploy): enhance post-deploy script with emojis and remove redundant messages

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-echo "=== Starting web app deployment ==="
-
 APP_DIR="/root/projects/betsweb"
 BUILD_DIR="/var/www/html"
 
 cd "$APP_DIR"
 
-echo "Installing dependencies..."
+echo "📦 Installing dependencies..."
 npm install
 
-echo "Building the application into $BUILD_DIR..."
+echo "🏗️ Building the application into $BUILD_DIR..."
 npm run --configuration production build -- --output-path="$BUILD_DIR"
-echo "Build completed."
-
-echo "=== Deployment finished successfully ==="
+echo "✅ Build completed."
